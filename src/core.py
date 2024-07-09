b5f4f82cec5a35bacf22da5eed879523fcdbef66
ituki0426 <yuzurihainori315@gmail.com>
2024-07-10 01:05:03 +0900

    initial commit

---
+++ src/core.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+core.py
+
+Created on `2024-07-10
+by ituki shibanuma
+st20097it@gm.ibaraki-ct.ac.jp
+"""
+import logging as log
+
+def main():
+    # TODO:
+    # run its processes
+    pass
+
+if __name__ == "__main__":
+    log.basicConfig(level=log.DEBUG,
+                    format='%(asctime)s %(message)s',
+                    datefmt="%b %d %H:%M:%S %Z")
+
+    main()
